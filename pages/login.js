ab52f80398f5e9aca2af0215ee2e8c95225182bd
Adrian Souto <adrian.souto@udc.es>
2022-08-05 17:57:03 +0200

    Adjust details on login

--- pages/login.js
+++ pages/login.js
@@ -9,19 +9,23 @@ export default function Login() {
   const [password, setPassword] = useState()
   const [remember, setRemember] = useState(false)
   const [callback, setCallback] = useState("/")
+  const [error, setError] = useState("")
 
   useEffect(() => {
     const query = new URLSearchParams(window.location.search)
+    const errorCallback = query.get('error')
     const callback = query.get('callbackUrl')
+
+    if(errorCallback){
+      if(errorCallback === "CredentialsSignin"){
+        setError("Usuario o contraseña incorrectos")
+      }
+    }
+
     if (callback) {
       setCallback(callback)
     }
-  },[callback])
-
-  function handleSubmit(e) {
-      e.preventDefault()
-      signIn('credentials', {username, password, remember, callbackUrl: callback })
-  }
+  },[])
 
   useEffect(() => {
     const autofilledUSer = document.getElementById('floatingInput').value
@@ -30,19 +34,38 @@ export default function Login() {
     if(autofilledPassword) setPassword(autofilledPassword)
   }, [])
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    signIn('credentials', {username, password, remember, callbackUrl: callback })
+  }
+
+  function handleChange(e) {
+    if(e.target.id === "floatingInput"){
+      setUserName(e.target.value)
+    }else{
+      setPassword(e.target.value)
+    }
+    setError("")
+  }
+
+
   return (
     <Layout>
         <form onSubmit={handleSubmit} style={{textAlign: "center"}} >
           <h1 className="h3 mb-3 fw-normal">Login</h1>
           <div className="form-floating">
-            <input type="username" className="form-control" id="floatingInput" placeholder="Username" onChange={e => setUserName(e.target.value)}/>
+            <input type="username" className="form-control" id="floatingInput" placeholder="Username" onChange={e => handleChange(e)}/>
             <label htmlFor="floatingInput">Username</label>
           </div>
           <div className="form-floating" >
-            <input id="floatingPassword" className="form-control" type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+            <input id="floatingPassword" className="form-control" type="password" placeholder="Password" onChange={e => handleChange(e)}/>
             <label htmlFor="floatingPassword">Password</label>
           </div>
           <button className="w-100 btn btn-lg btn-primary mt-3" type="submit">Login</button>
+          {
+          error && 
+          <p style={{color: "red", fontSize: "small"}}>{error}</p>
+          }
           <div>
             <input className="mt-3" type="checkbox" id="remember" onChange={e => setRemember(e.target.checked)}/>
             <label htmlFor="remember" style={{paddingLeft: "5px"}}>Remember</label>
